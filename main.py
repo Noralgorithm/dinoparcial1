4c6527ecb8f0b5a4a898da9a5d5c85f58b178fbf
NØRA <87492801+Noralgorithm@users.noreply.github.com>
2025-05-05 18:13:38 -0400

    chore: change title

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.set_page_config(
 )
 
 # Título principal
-st.title("Sistema Inteligente de Control de Inventario - ConsumoMax")
+st.title("Sistema Inteligente de Control de Inventario")
 
 # Filtros laterales
 st.sidebar.header("Filtros de búsqueda")
